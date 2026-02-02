06a11a8529f7fcb061daaae85ff1ce4cb81eb3b0
vanish_gold <mitrofanovvana20082@gmail.com>
2026-02-02 10:00:55 +0100

    Initial commit

---
+++ src/test/java/com/example/SorokinSpringBoot/SorokinSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SorokinSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorokinSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
