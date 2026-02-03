ed4591fae8f27a15b1aaa4137e2ccd43df36c605
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-02-03 12:44:52 +0100

    feat: create loggers

--- src/main/java/com/example/SorokinSpringBoot/ReservationController.java
+++ src/main/java/com/example/SorokinSpringBoot/ReservationController.java
@@ -1,6 +1,7 @@
 package com.example.SorokinSpringBoot;
 
-import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import java.util.List;
 
 @RestController
 public class ReservationController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ReservationController.class);
 
     private final ReservationService reservationService;
 
@@ -20,12 +23,13 @@ public class ReservationController {
     public Reservation getReservationById(
             @PathVariable("id") Long id
     ){
-        System.out.println("Reservation with id " + id);
+        logger.info("Called getReservationById " + id);
         return reservationService.getReservationById(id);
     }
 
     @GetMapping()
     public List<Reservation> getAllReservations(){
+        logger.info("Called getAllReservations");
         return reservationService.findAllReservations();
     }
 }
