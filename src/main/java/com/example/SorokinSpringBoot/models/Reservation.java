3e8abee7caafd895f61e6e50c20eb8b0d9107d29
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-02-10 20:03:59 +0100

    feat: add annotations for Reservation.java

--- src/main/java/com/example/SorokinSpringBoot/models/Reservation.java
+++ src/main/java/com/example/SorokinSpringBoot/models/Reservation.java
@@ -1,14 +1,24 @@
 package com.example.SorokinSpringBoot.models;
 
 import com.example.SorokinSpringBoot.enums.ReservationStatus;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 
 import java.time.LocalDate;
 
 public record Reservation(
+        @Null
         Long id,
+        @NotNull
         Long userId,
+        @NotNull
         Long roomId,
+        @FutureOrPresent
+        @NotNull
         LocalDate startDate,
+        @FutureOrPresent
+        @NotNull
         LocalDate endDate,
         ReservationStatus status
 ) {
